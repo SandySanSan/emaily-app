191f48c09ecbc7dca811eaea24b4939493f55ea0
Sandy <tottati@hotmail.com>
2020-03-07 15:24:44 +0100

    setting cookie, handling logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,4 +9,16 @@ module.exports = app => {
     );
 
     app.get('/auth/google/callback', passport.authenticate('google'));
+
+    app.get('/api/logout', (req, res) => {
+        // logout() is attached automatically to the req object by passport
+        // takes the cookie that contains the user's id and kills the id that's in there
+        req.logout();
+        res.send(req.user);
+    });
+    // req > incoming request
+    // res > outgoing response
+    app.get('/api/current_user', (req, res) => {
+        res.send(req.user);
+    });
 };
--- services/passport.js
+++ services/passport.js
@@ -6,6 +6,17 @@ const keys = require('../config/keys');
 // pull model out of mongoose (single argument)
 const User = mongoose.model('users');
 
+// user >  Model instance of mongoose
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+        done(null, user);
+    });
+});
+
 // initialize passport strategy
 passport.use(
     new GoogleStrategy(
@@ -15,9 +26,22 @@ passport.use(
             callbackURL: '/auth/google/callback',
         },
         (accessToken, refreshToken, profile, done) => {
-            // create a new instance of the Model Class
-            // and save it to the db
-            new User({ googleId: profile.id }).save();
+            // return promise
+            User.findOne({ googleId: profile.id }).then(existingUser => {
+                if (existingUser) {
+                    // record found with the given profile id
+                    done(null, existingUser);
+                    // done(), callback, tells passport the auth flow is complete
+                    // null -> no error
+                } else {
+                    // existingUser is null,
+                    // create as new instance of the Model Class
+                    // and save it to the db (create a new Model instance)
+                    new User({ googleId: profile.id })
+                        .save()
+                        .then(user => done(null, user));
+                }
+            });
         }
     )
 );
